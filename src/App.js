e9d43f75fdb470c3574fb9bcb1565981492a6096
dirrialsss <dirrialss@gmail.com>
2025-12-02 18:20:39 +0200

    updating path

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import GoodsCard from './components/GoodsCard';
 import './App.css';
 
 const goods = [
-  { id: 1, name: "Zero One зі смаком печивa", price: "50 грн", img: "/images/bar1.png" },
-  { id: 2, name: "Zero One малиновий чізкейк", price: "50 грн", img: "/images/bar2.png" },
-  { id: 3, name: "Zero One арахісова паста", price: "50 грн", img: "/images/bar3.png" },
-  { id: 4, name: "Zero One зі смаком кави", price: "50 грн", img: "/images/bar4.png" },
-  { id: 5, name: "FitWin фісташка та крем", price: "65 грн", img: "/images/bar5.png" },
-  { id: 6, name: "FitWin печиво з кремом", price: "65 грн", img: "/images/bar6.png" },
+  { id: 1, name: "Zero One зі смаком печивa", price: "50 грн", img: "images/bar1.png" },
+  { id: 2, name: "Zero One малиновий чізкейк", price: "50 грн", img: "images/bar2.png" },
+  { id: 3, name: "Zero One арахісова паста", price: "50 грн", img: "images/bar3.png" },
+  { id: 4, name: "Zero One зі смаком кави", price: "50 грн", img: "images/bar4.png" },
+  { id: 5, name: "FitWin фісташка та крем", price: "65 грн", img: "images/bar5.png" },
+  { id: 6, name: "FitWin печиво з кремом", price: "65 грн", img: "images/bar6.png" },
 ];
 
 function App() {
